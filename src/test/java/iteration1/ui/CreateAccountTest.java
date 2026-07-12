40d1d0757bb10046e6eeec2933a46b3e3c4ec194
ruslan <ru_a@mail.ru>
2026-07-12 23:12:48 +0300

    UI tests iteration 1

--- src/test/java/iteration1/ui/CreateAccountTest.java
+++ src/test/java/iteration1/ui/CreateAccountTest.java
@@ -5,7 +5,6 @@ import models.CreateUserRequest;
 import org.junit.jupiter.api.Test;
 import requests.steps.AdminSteps;
 import requests.steps.UserSteps;
-import requests.ui.pages.BankAlert;
 import requests.ui.pages.UserDashboard;
 
 import java.util.List;
@@ -13,22 +12,21 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CreateAccountTest extends BaseUiTest {
+
     @Test
     public void userCanCreateAccountTest() {
         CreateUserRequest user = AdminSteps.createUser();
-
         authAsUser(user);
+
+        List<CreateAccountResponse> accountsBefore = new UserSteps(user.getUsername(), user.getPassword())
+                .getAllAccounts();
 
         new UserDashboard().open().createNewAccount();
 
-        List<CreateAccountResponse> createdAccounts = new UserSteps(user.getUsername(), user.getPassword())
+        List<CreateAccountResponse> accountsAfter = new UserSteps(user.getUsername(), user.getPassword())
                 .getAllAccounts();
 
-        assertThat(createdAccounts).hasSize(1);
-
-        new UserDashboard().checkAlertMessageAndAccept
-                (BankAlert.NEW_ACCOUNT_CREATED.getMessage() + createdAccounts.getFirst().getAccountNumber());
-
-        assertThat(createdAccounts.getFirst().getBalance()).isZero();
+        assertThat(accountsAfter).hasSize(accountsBefore.size() + 1);
+        assertThat(accountsAfter.getLast().getBalance()).isZero();
     }
 }
